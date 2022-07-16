b75d95e300b7e15343658e2ab2edd20146469b9c
Benito1001 <bendid2103@gmail.com>
2022-07-16 17:27:36 +0200

    Removed and renamed image

--- src/main.rs
+++ src/main.rs
@@ -53,17 +53,17 @@ fn main() {
 
     let x_ray = Array::from(x_vec);
     let y_ray = Array::from(y_vec);
-    
+
     let err_f = calc_err(x_ray.to_owned(), y_ray.to_owned(), f);
     let grad_err_f = calc_grad_err(x_ray.to_owned(), y_ray.to_owned(), f, grad_f);
     let inv_hess_err_f = calc_inv_hess_err(x_ray.to_owned(), y_ray.to_owned(), f, hess_f, outer_f);
 
-    
+
     let now = Instant::now();
     let initial_params = Array::ones(num_params);
     let optimal_params = combined_descent(&initial_params, &*err_f, &*grad_err_f, &*inv_hess_err_f, true);
     println!("Descent took {} ms", (now.elapsed().as_nanos() as f64) / 1_000_000_f64);
     println!("Got the parameters {}", optimal_params);
 
-    plot(&x_ray, &y_ray, f, &optimal_params, "test.png")
+    plot(&x_ray, &y_ray, f, &optimal_params, "result.png")
 }
